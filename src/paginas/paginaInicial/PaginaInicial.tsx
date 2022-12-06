8ca99873e4b27b1ae1f5f9f1d1ed69857f75ac42
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2022-12-06 16:58:18 -0300

    subindo primeira versão chatbot

--- src/paginas/paginaInicial/PaginaInicial.tsx
+++ src/paginas/paginaInicial/PaginaInicial.tsx
@@ -5,23 +5,73 @@ import "./PaginaInicial.css";
 function PaginaInicial() {
   return (
     <>
-      <div>
-        <div className="bk-paginicial">
-          <img src="https://imgur.com/8hZkgKC.png" alt="crianças sorrindo" />
+      <div className="div-paginicial">
+        <div>
+          <div className="div-home">
+            <img
+              src="https://imgur.com/qXqRyha.png"
+              alt=""
+              className="img-home"
+            />
+            <h2>JUNTOS SOMOS MAIS</h2>
+            <Link to="/login" className="button-inicial">
+              <button>Quero Ajudar</button>
+            </Link>
+            <p>Com a Bem Fazer ficou mais fácil de fazer o BEM!</p>
+            <p>Faça a diferença, vamos mudar o mundo juntos.</p>
+          </div>
         </div>
-        
-        <Link to='/login'>
-        <a className="fancy" href="#">
-          <span className="top-key"></span>
-          <span className="text">Saiba mais</span>
-          <span className="bottom-key-1"></span>
-          <span className="bottom-key-2"></span>
-        </a>
-        </Link>
+      </div>
 
-        <div className="img-inicial">
-        <img src="https://imgur.com/8jjQm8b.png" alt="" />
+      <div className="div-segundapag">
+        <img src="https://imgur.com/sRo0AQb.png" alt="" className="logo-ong" />
+        <p>
+          Como pessoa doadora você pode encontrar instituições
+          <br />
+          que precisam de doações.
+        </p>
+
+        <img src="https://imgur.com/1RhhEm1.png" alt="" className="logo-pf" />
+        <p>
+          Como ONG poderá indicar qual tipo de serviço <br /> está precisando e
+          como as pessoas podem ajudar.{" "}
+        </p>
+      </div>
+
+      <div className="div-terceirapag">
+        <h3>
+          Pensando em ampliar os tipos de doações, a Bem Fazer tem como
+          objetivo facilitar para que qualquer pessoa pratique o bem para a
+          sociedade da forma que puder, como:
+        </h3>
+        <div className="p-icone">
+          <div className="dinheiro">
+            <img
+              src="https://imgur.com/pdRK77x.png" alt="Ícone de dinheiro" />
+            <br />
+            <p>Dinheiro:</p>
+            <p>
+              Com a doação em dinheiro, você pode ajudar as ONG'S a manter os seus projetos que mudam vidas de muitas pessoas.
+            </p>
+          </div>
+          <div className="tempo">
+            <img src="https://imgur.com/3nipgx6.png" alt="Ícone de relógio" />
+            <br />
+            <p>Tempo:</p>
+            <p>
+              Dedique uma parte do seu tempo para instituições que necessitam de voluntários para manutenção e gestão do local.
+            </p>
+          </div>
+          <div className="material">
+            <img src="https://imgur.com/Bi7BYM1.png" alt="Ícone de martelo" />
+            <br />
+            <p>Materiais:</p>
+            <p>
+              Com materiais que você tem em sua casa e não utiliza mais, alguma ONG'S podem estar precisando deste material, alguns deles como: cadernos, livros, roupas e etc.
+            </p>
+          </div>
         </div>
+
       </div>
     </>
   );
